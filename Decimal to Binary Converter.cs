ab7e9f97a9cb2ce05850fd3901ae39cfd8fd30eb
nikldev0 <57417252+nikldev0@users.noreply.github.com>
2021-02-21 20:45:35 +0000

    feat: decimal to binary value conversion program

---
+++ Decimal to Binary Converter.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace NumberConversion1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            string Binary=string.Empty;
+         
+           int Decimal,rem,n,i,L;
+            Console.WriteLine("Decimal to Binary Number Conversion");
+            Console.WriteLine("Enter the Decimal Number");
+            Decimal = Convert.ToInt32(Console.ReadLine());
+          
+            for(n=Decimal;n!=0;n=n/2)
+            {
+                rem = n % 2;
+                Binary = Binary + rem.ToString();
+           }
+            Console.WriteLine("The Binary Output Value is:");
+           for (i = Binary.Length - 1; i >= 0; i--)
+            {
+                Console.Write(Binary[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
